dabc21000e781f0ac3bde67e32122894e6f031f6
Mohammad Nimrawi <mohammad.alnimrawi@makane.com>
2022-08-07 12:07:16 +0300

    done

---
+++ app.js
@@ -0,0 +1,90 @@
+// Global component
+Vue.component('my-component', {
+    data: function () {
+        return {
+            count: 0,
+        }
+    },
+    props: ['name', 'id'],
+    template: `<p @click="count++">Test Component {{count}} hello {{name}}
+                 your id is {{id}}</p>`,
+})
+
+// Local component
+const myItem = {
+    data: function () {
+        return {
+            likes: 0,
+        }
+    },
+    props: ['name', 'id'],
+    template: `<p @click="likes++">Test item {{likes}} hello {{name}}
+                 your id is {{id}}</p>`,
+}
+
+// Vue app
+var app = new Vue({
+    el: '#app',
+
+    data: {
+        message: 'Hello Vue!',
+        loop: [111, 222, 333, 411, 511, 611, 711, 118, 119, 1110],
+        date: new Date(),
+        age: 60,
+        show: true,
+        html: '<span style="color:red">This is red</span>',
+        imgSrc: '',
+        bind: '',
+        posts: posts,
+        jod: 0,
+        riyal: 0,
+        hour: 0,
+        minute: 0,
+        second: 0,
+    },
+
+    methods: {
+        changeMessage: function () {
+            console.log(data)
+            this.message = 'Hello Vue!!!!!!!!!!!!!'
+        },
+        UpdateDate: function () {
+            this.date = new Date()
+        },
+        fixImg: function () {
+            this.imgSrc = 'levi.jpg'
+        }
+    },
+    components: {
+        myItem: myItem,
+    },
+    computed: {
+        calcAge: function () {
+            console.log('calcAge') //it will be executed one time
+            return this.age * 2
+        }
+    },
+    watch: {
+        jod: function (newVal) {
+            console.log('jod changed')
+            this.riyal = newVal * 5
+        },
+        riyal: function (newVal) {
+            console.log('riyal changed')
+            this.jod = newVal / 5
+        },
+        hour: function (newVal) {
+            console.log('hour changed')
+            this.minute = newVal * 60
+        },
+        minute: function (newVal) {
+            console.log('minute changed')
+            this.hour = newVal / 60
+            this.second = newVal * 60
+        },
+        second: function (newVal) {
+            console.log('second changed')
+            this.minute = newVal / 60
+        }
+    }
+})
