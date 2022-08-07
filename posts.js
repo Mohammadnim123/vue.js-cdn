1f86410463d0e45fb76688f6ca398db9c2b9231a
Mohammad Nimrawi <mohammad.alnimrawi@makane.com>
2022-08-07 11:41:20 +0300

    done

---
+++ posts.js
@@ -0,0 +1,20 @@
+const posts = [
+    {
+        "_id": "62ed78c0da9cbbefe0676e28",
+        "age": 39,
+        "eyeColor": "blue",
+        "name": "Miranda Ford",
+        "gender": "female",
+        "company": "COMBOGENE",
+        "email": "mirandaford@combogene.com"
+    },
+    {
+        "_id": "62ed78c065997181c206de7b",
+        "age": 26,
+        "eyeColor": "green",
+        "name": "Welch Vance",
+        "gender": "male",
+        "company": "QUILCH",
+        "email": "welchvance@quilch.com"
+    }
+]
